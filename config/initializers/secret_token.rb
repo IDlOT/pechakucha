2888df59b6929dc4135231669a4298d903444113
Tammer Abiyu <tammerabiyu@gmail.com>
2014-04-07 19:48:19 -0400

    ichiban no committo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pechakucha::Application.config.secret_key_base = '0bba857e198c2b463aebc07e44d639267d9bbde94cc7c3d90698e783b40aec59118f03a27189f30177c8a1d8f58048eb86693add4accdd262cdcd7e5dce4e6be'
